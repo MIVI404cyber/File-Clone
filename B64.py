c7e4a999bf91aa610cda5eb1a74e6dc5866ed6f3
MR.MIVI 404 Cyber <112892396+MIVI404cyber@users.noreply.github.com>
2023-01-12 14:17:56 +0600

    Add files via upload

---
+++ B64.py
@@ -0,0 +1,19 @@
+#<\>!python3.11
+#-------------------Dont Edit This Script-----------------#
+import os,platform,time
+
+bitt=platform.architecture()[0]
+
+if bitt=="64bit":
+    os.system('clear');print('[!] Your Device is 64 bit');time.sleep(1);print('\n\n[!] Your Python Version :');time.sleep(1);os.system('python --version')
+    time.sleep(2)
+    import B64.main
+
+else:
+
+    print('\nYOUR DEVICE 32 BIT NOT SUPPORT')
+
+#---------------------------------------------------------#
+#                    THIS TOOL OWNED BY
+#                  •              MR.MIVI             •
+#---------------------------------------------------------#
