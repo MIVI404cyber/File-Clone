3d5cccd69b7355f1b7e11bce1a195a6f9cc6618d
MR.MIVI 404 Cyber <112892396+MIVI404cyber@users.noreply.github.com>
2023-01-12 14:19:20 +0600

    Update B64.py

--- B64.py
+++ B64.py
@@ -4,10 +4,10 @@ import os,platform,time
 
 bitt=platform.architecture()[0]
 
-if bitt=="64bit":
+if bitt=="32bit":
     os.system('clear');print('[!] Your Device is 64 bit');time.sleep(1);print('\n\n[!] Your Python Version :');time.sleep(1);os.system('python --version')
     time.sleep(2)
-    import B64.main
+    import B32.main
 
 else:
 
