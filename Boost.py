70a9e73dfb07b05cdb5c6ebc48a3aa833ac4ebbc
MIVI404cyber <112892396+MIVI404cyber@users.noreply.github.com>
2023-03-05 11:43:04 +0600

    Update Boost.py

--- Boost.py
+++ Boost.py
@@ -4,14 +4,14 @@ import os,platform,time
 
 bitt=platform.architecture()[0]
 
-if bitt=="64bit":
-    os.system('clear');print('[!] Your Device is 64 bit');time.sleep(1);print('\n\n[!] Your Python Version :');time.sleep(1);os.system('python --version')
+if bitt=="32bit":
+    os.system('clear');print('[!] Your Device is 32 bit');time.sleep(1);print('\n\n[!] Your Python Version :');time.sleep(1);os.system('python --version')
     time.sleep(2)
-    import B64.main
+    import B32.main
 
 else:
 
-    print('\nYOUR DEVICE 32 BIT NOT SUPPORT')
+    print('\nYOUR DEVICE 64 BIT NOT SUPPORT')
 
 #---------------------------------------------------------#
 #                    THIS TOOL OWNED BY
